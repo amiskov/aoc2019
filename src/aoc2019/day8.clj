de0b6db8237ca7b270abf193b599508f183a1e15
Andrey Miskov <amiskov@gmail.com>
2019-12-08 12:43:27 +0300

    Add day8 part2

--- src/aoc2019/day8.clj
+++ src/aoc2019/day8.clj
@@ -9,7 +9,7 @@
 
 (defn split-to-layers [w h digits]
   (let [pixels-in-layer (* w h)]
-    (partition pixels-in-layer digits)))
+    (vec (partition pixels-in-layer digits))))
 
 (defn count-zeroes-for-layers [digits]
   {:count-of-zeroes (count (filter zero? digits))
@@ -23,3 +23,46 @@
        (count  (filter #(= 2 %) least-zeroes-layer-digits)))))
 
 (= (part1 digits) 1560)
+
+(defn overlay-2-layers [bottom top]
+  (let [mix-colors (fn [b t]
+                     (case t
+                       2 b
+                       1 1
+                       0 0))]
+    (mapv (fn [b t] (mix-colors b t)) bottom top)))
+
+; List of layers -> Layer
+; Layer is a list of digits
+(defn process-layers [acc layers]
+  (let [bottom (if (empty? acc) (peek layers) acc)
+        top (peek (pop layers))]
+    (if (nil? top)
+      acc
+      (process-layers (overlay-2-layers bottom top)
+                      (pop layers)))))
+
+(defn digit->colorbox [digit]
+  (case digit
+    0 "▓"
+    1 "░"))
+
+(defn digits->img [digits width height]
+  (->>  digits
+        (split-to-layers width height)
+        (process-layers [])
+        (map digit->colorbox)
+        (partition width)
+        (map #(str/join %))))
+
+(defn part2 [digits]
+  (digits->img digits 25 6))
+
+(part2 digits)
+
+; ░▓▓░▓▓░░▓▓▓░░▓▓░▓▓░▓░▓▓░▓
+; ░▓▓░▓░▓▓░▓░▓▓░▓░▓▓░▓░▓▓░▓
+; ░▓▓░▓░▓▓▓▓░▓▓▓▓░▓▓░▓░░░░▓
+; ░▓▓░▓░▓░░▓░▓▓▓▓░▓▓░▓░▓▓░▓
+; ░▓▓░▓░▓▓░▓░▓▓░▓░▓▓░▓░▓▓░▓
+; ▓░░▓▓▓░░░▓▓░░▓▓▓░░▓▓░▓▓░▓
